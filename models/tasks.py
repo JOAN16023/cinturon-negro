4531f12d0b6aee011659e6b197120f4c5d59f10d
joan <joan.tapia.hermosilla@alumnos.sip.cl>
2024-11-13 20:49:20 -0300

    1

---
+++ models/tasks.py
@@ -0,0 +1,45 @@
+from config.db import connectToMySQL
+
+class Tasks:
+    def __init__(self, data):
+        self.id = data["user_id"]
+        self.name = data["name"]
+        self.status = data["status"]
+        self.date = data["date"]
+        self.usuario_id = data["id"]
+        
+    @classmethod
+    def select_all(cls):
+        query = "SELECT * FROM Tasks"
+        results = connectToMySQL('cd_exam3').query_db(query)
+        tasks = []
+        for task in results:
+            tasks.append(cls(task))
+        return tasks
+    
+    @classmethod
+    def select_one(cls, user_id):
+        query = f"SELECT * FROM Tasks WHERE user_id = {user_id}"
+        results = connectToMySQL('cd_exam3').query_db(query)
+        tasks = []
+        for task in results:
+            tasks.append(cls(task))
+        return tasks
+    
+    @classmethod
+    def insert(cls, name, status, date):
+        query = f"INSERT INTO Tasks (nombre) VALUES ('{name}', '{status}', '{date}')"
+        result = connectToMySQL('cd_exam3').query_db(query)
+        return result
+    
+    @classmethod
+    def update(cls, usuario_id, name, status, date):
+        query = f"UPDATE Tasks SET name='{name}', status='{status}', date='{date}' WHERE usuario_id={usuario_id}"
+        result = connectToMySQL('cd_exam3').query_db(query)
+        return result
+    
+    @classmethod
+    def delete_one(cls, usuario_id):
+        query = f"DELETE FROM Tasks WHERE usuario_id={usuario_id}"
+        result = connectToMySQL('cd_exam3').query_db(query)
+        return result
--- models/usuarios.py
+++ models/usuarios.py
@@ -11,13 +11,13 @@ class Usuario:
     @classmethod
     def insert_usuario(cls, first_name, last_name, email, password):
         query = f"INSERT INTO Usuario (first_name, last_name, email, password) VALUES ('{first_name}', '{last_name}', '{email}', '{password}');"
-        results = connectToMySQL("cd_exam").query_db(query)
+        results = connectToMySQL("cd_exam3").query_db(query)
         return results
     
     @classmethod
     def select_correo(cls, email):
         query = f"SELECT * FROM Usuario WHERE email = '{email}'"
-        results = connectToMySQL("cd_exam").query_db(query)
+        results = connectToMySQL("cd_exam3").query_db(query)
         usuarios = []
         for usuario in results:
             usuarios.append(cls(usuario))
